b2338669d8fdab995cf8c2f7996379b1ba896eaf
adefrol <adefrolx@gmail.com>
2024-03-15 00:13:46 +0800

    seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,11 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Главная",
   description: "Главная",
+  openGraph : {
+    type : "website",
+    title : "illumedWeb",
+    description : "Команда экспертов в разработке сайтов и мобильных приложений."
+  }
 };
 
 export default function RootLayout({
@@ -18,10 +23,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="ru" >
       <body className={`${inter.className}`}>
         <Providers>
-          <div className="py-[40px] bg-[url('/bgmain.png')] transition-all bg-cover min-h-screen dark:bg-[url('/bgmain-dark.webp')]">
+          <div className="py-[40px] bg-[url('/bgmain.png')] transition-all bg-cover min-h-screen dark:bg-[url('/bgmain-dark.jpg')]">
             <div className="max-w-[1100px] mx-auto">
               <Header />
               <div className="mt-[80px] max-[639px]:mx-[20px]">{children}</div>
