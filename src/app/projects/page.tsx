0880f61b806d19220ec3f615d014b2e4a452d23b
3N14C <3n14c@vk.com>
2024-03-14 23:40:14 +0800

    Update page.tsx

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,7 +1,9 @@
 import { PageTitle } from "@/components/PageTitle";
+import { CardBody, CardContainer, CardItem } from "@/components/ui/3d-card";
 import { projects } from "@/lib/projects";
 import { MoveUpRight } from "lucide-react";
 import { NextPage } from "next";
+import Image from "next/image";
 import Link from "next/link";
 
 interface Props {}
@@ -14,27 +16,56 @@ const Page: NextPage<Props> = ({}) => {
       <div className="mt-[50px]">
         <div className="">
           {projects.length ? (
-            <div className="grid grid-cols-3 lg:gap-[60px] max-[639px]:flex max-[639px]:flex-col max-[639px]:gap-[40px]">
+            <div className="flex flex-wrap lg:gap-[60px] max-[639px]:flex max-[639px]:flex-col max-[639px]:gap-[40px]">
               {projects.map((project) => (
-                <Link
-                  href={`/projects/${project.id}`}
-                  key={project.id}
-                  className="flex flex-col gap-[5px] max-[639px]:gap-0"
-                >
-                  <img
-                    src={project.img}
-                    className="w-[350px] h-[200px] max-[639px]:h-full max-[639px]:w-full"
-                  />
-
-                  <p className="text-black/50 font-semibold">
-                    {project.dateTime}
-                  </p>
-
-                  <p className="font-bold text-[25px] flex items-center gap-1">
-                    {project.title}
-                    <MoveUpRight size={20} />
-                  </p>
-                </Link>
+                <CardContainer className="inter-var">
+                  <CardBody
+                    className="bg-gray-50 backdrop-blur-md bg-opacity-10 relative group/card  dark:hover:shadow-2xl 
+                  dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:backdrop-blur-md dark:bg-opacity-25 dark:border-white/[0.2] border-black/[0.1] 
+                  w-auto sm:w-[30rem] h-auto rounded-xl p-6 border"
+                  >
+                    <CardItem
+                      translateZ="50"
+                      className="text-xl font-bold text-neutral-600 dark:text-white"
+                    >
+                      {project.title}
+                    </CardItem>
+                    <CardItem
+                      as="p"
+                      translateZ="60"
+                      className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+                    >
+                      {project.dateTime}
+                    </CardItem>
+                    <CardItem translateZ="100" className="w-full mt-4">
+                      <Image
+                        src={project.img}
+                        height={1000}
+                        width={1000}
+                        className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
+                        alt="thumbnail"
+                      />
+                    </CardItem>
+                    <div className="flex justify-between items-center mt-20">
+                      <CardItem
+                        translateZ={20}
+                        as={Link}
+                        href="https://twitter.com/mannupaaji"
+                        target="__blank"
+                        className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
+                      >
+                        Подробнее →
+                      </CardItem>
+                      <CardItem
+                        translateZ={20}
+                        as="button"
+                        className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
+                      >
+                        <Link href={`/projects/${project.id}`}>Подробнее</Link>
+                      </CardItem>
+                    </div>
+                  </CardBody>
+                </CardContainer>
               ))}
             </div>
           ) : (
