582186692ed8455a161c5541bbfeb3969ba3fb69
3N14C <3n14c@vk.com>
2024-03-15 20:29:06 +0800

    import floating navbar

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,11 +1,14 @@
 import { NextPage } from "next";
 import { Content } from "./_components/Content";
+import { FloatingNav } from "@/components/ui/floating-navbar";
+import { navbar } from "@/constants/navbar";
 
 interface Props {}
 
 const Page: NextPage<Props> = ({}) => {
   return (
     <>
+    <FloatingNav navItems={navbar} />
       <Content />
     </>
   );
