e8268cbc1e2df55e93fe851cd2eca2ebeed354d5
3N14C <3n14c@vk.com>
2024-03-17 01:22:33 +0800

    Create reviews.ts

---
+++ src/lib/reviews.ts
@@ -0,0 +1,48 @@
+import { IReviews } from "@/interfaces/reviews.interface";
+
+export const reviews: IReviews[] = [
+  {
+    id: 1,
+    review: "review",
+    author: "author",
+    projectLink: "projectLink",
+
+    dateTime: "datetime",
+  },
+
+  {
+    id: 2,
+    review: "review",
+    author: "author",
+    projectLink: "projectLink",
+
+    dateTime: "datetime",
+  },
+
+  {
+    id: 3,
+    review: "review",
+    author: "author",
+    projectLink: "projectLink",
+
+    dateTime: "datetime",
+  },
+
+  {
+    id: 4,
+    review: "review",
+    author: "author",
+    projectLink: "projectLink",
+
+    dateTime: "datetime",
+  },
+
+  {
+    id: 5,
+    review: "review",
+    author: "author",
+    projectLink: "projectLink",
+
+    dateTime: "datetime",
+  },
+];
