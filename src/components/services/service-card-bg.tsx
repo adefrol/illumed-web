48ec75ef8c24851453ace26c6e74f74a6069c0ef
3N14C <3n14c@vk.com>
2024-07-25 01:17:38 +0800

    updated

--- src/components/services/service-card-bg.tsx
+++ src/components/services/service-card-bg.tsx
@@ -11,7 +11,7 @@ export const ServiceCardBg: FC<IProps> = ({ children }) => {
     <div className="gradient-border-service-card group/bgCard rounded-3xl w-[90%]">
       <div
         className="w-full h-96 rounded-[30px] bg-gradient-to-tr from-black/50 to-gray-50/10 
-      "
+      group-hover/bgCard:from-[#2c1b3a]/60 group-hover/bgCard:to-primary/10"
       >
         {children}
       </div>
